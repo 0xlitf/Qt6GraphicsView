c23b97947c94a41f1fa5be7b9b8e2c0d91a32123
0xlitf <0xlitf@gmail.com>
2023-12-05 11:59:48 +0800

    ·

--- UsingGraphics/src/focuspoint.h
+++ UsingGraphics/src/focuspoint.h
@@ -18,22 +18,33 @@ public:
         Center,
         UserDefined
     };
-    QMap<Position, Qt::CursorShape> m_cursorMap;
+    QMap<Position, Qt::CursorShape> m_cursorMap{
+        {Position::LeftTop, Qt::SizeFDiagCursor},
+        {Position::RightBottom, Qt::SizeFDiagCursor},
+        {Position::LeftBottom, Qt::SizeBDiagCursor},
+        {Position::RightTop, Qt::SizeBDiagCursor},
+        {Position::Top, Qt::SizeVerCursor},
+        {Position::Bottom, Qt::SizeVerCursor},
+        {Position::Left, Qt::SizeHorCursor},
+        {Position::Right, Qt::SizeHorCursor},
+        {Position::Center, Qt::SizeAllCursor},
+        {Position::UserDefined, Qt::ArrowCursor}
+    };
 
     FocusPoint();
     FocusPoint(int xpos, int ypos, Position position) noexcept
         : QPoint(xpos, ypos)
         , m_position{position} {
-        m_cursorMap.insert(Position::LeftTop, Qt::SizeFDiagCursor);
-        m_cursorMap.insert(Position::RightBottom, Qt::SizeFDiagCursor);
-        m_cursorMap.insert(Position::LeftBottom, Qt::SizeBDiagCursor);
-        m_cursorMap.insert(Position::RightTop, Qt::SizeBDiagCursor);
-        m_cursorMap.insert(Position::Top, Qt::SizeVerCursor);
-        m_cursorMap.insert(Position::Bottom, Qt::SizeVerCursor);
-        m_cursorMap.insert(Position::Left, Qt::SizeHorCursor);
-        m_cursorMap.insert(Position::Right, Qt::SizeHorCursor);
-        m_cursorMap.insert(Position::Center, Qt::SizeAllCursor);
-        m_cursorMap.insert(Position::UserDefined, Qt::ArrowCursor);
+        // m_cursorMap.insert(Position::LeftTop, Qt::SizeFDiagCursor);
+        // m_cursorMap.insert(Position::RightBottom, Qt::SizeFDiagCursor);
+        // m_cursorMap.insert(Position::LeftBottom, Qt::SizeBDiagCursor);
+        // m_cursorMap.insert(Position::RightTop, Qt::SizeBDiagCursor);
+        // m_cursorMap.insert(Position::Top, Qt::SizeVerCursor);
+        // m_cursorMap.insert(Position::Bottom, Qt::SizeVerCursor);
+        // m_cursorMap.insert(Position::Left, Qt::SizeHorCursor);
+        // m_cursorMap.insert(Position::Right, Qt::SizeHorCursor);
+        // m_cursorMap.insert(Position::Center, Qt::SizeAllCursor);
+        // m_cursorMap.insert(Position::UserDefined, Qt::ArrowCursor);
     }
 
     Position position() const;
